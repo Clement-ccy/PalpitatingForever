1940be9084ab1ae2e62b8e89e9beba8d0d13080f
Clement-ccy <319788356@qq.com>
2024-05-19 16:30:19 +0800

    2024.05.19

---
+++ src/components/TopMenu.vue
@@ -0,0 +1,83 @@
+<template>
+  <nav class="top-menu" :style="{ transform: topMenuTransform }">
+    <ul>
+      <span style="margin-right: 30vw">PalpitatingForever</span>
+      <li><a href="#portfolio">Portfolio</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#blog">Blog</a></li>
+      <li><a href="#contact">Contact</a></li>
+    </ul>
+    <hr />
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "TopMenu",
+  data() {
+    return {
+      topMenuTransform: "translateY(-100%)",
+    };
+  },
+  mounted() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  unmounted() {
+    window.removeEventListener("scroll", this.handleScroll);
+  },
+  methods: {
+    handleScroll() {
+      if (window.scrollY > 200) {
+        this.topMenuTransform = "translateY(0)";
+      } else {
+        this.topMenuTransform = "translateY(-100%)";
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.top-menu {
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 1000;
+  transition: transform 0.5s ease-in-out;
+  backdrop-filter: blur(3px);
+}
+
+.top-menu ul {
+  list-style: none;
+  padding: 0;
+  margin-top: 1rem;
+  text-align: center;
+}
+
+.top-menu li {
+  display: inline-block;
+  margin: 0 10px;
+}
+.top-menu span {
+    font-size: 1.333rem;
+}
+.top-menu a {
+  color: var(--c-text-selected);
+  text-decoration: none;
+  font-size: 1rem;
+  transition: color 0.3s ease-in-out;
+}
+
+.top-menu a:hover {
+  color: var(--c-red);
+}
+
+hr {
+  width: 90vw;
+  align-self: center;
+  margin-top: 1rem;
+}
+</style>
