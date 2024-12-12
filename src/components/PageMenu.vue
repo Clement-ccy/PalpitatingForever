4be36bc594a66b8dc6d8f94199491c76c473c7e3
Palpitating-Forever <319788356@qq.com>
2024-12-12 15:50:03 +0800

    12.12 15:50

--- src/components/PageMenu.vue
+++ src/components/PageMenu.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="menu" :class="{ active: isActive }">
+  <div class="menu">
+    <div class="float"></div>
     <div v-if="menuState === 'default'" class="menu-buttons">
       <button @click="changeContent('/whatido')">What I Do</button>
       <button @click="changeContent('/whoiam')">Who I Am</button>
@@ -22,8 +23,7 @@ import { gsap } from "gsap";
 export default {
   data() {
     return {
-      menuState:'default',
-      isActive: false,
+      menuState: 'default',
       currentLabel: '123456',
     };
   },
@@ -36,22 +36,26 @@ export default {
     },
     activateMenu(label) {
       this.currentLabel = label;
-      this.isActive = true;
-      this.animateToActive();
+
+      // 使用 GSAP 实现弹性变形到活动状态
+      gsap.to(".menu", {
+        duration: 0.5,
+        width: "calc-size(auto)",
+        ease: "elastic.out(1, 0.5)",
+        onStart: () => {
+          this.menuState = "breadCrumb"
+        }
+      });
     },
     resetMenu() {
       this.menuState = "default"
-      this.isActive = false;
-      this.animateToDefault();
-    },
-    animateToActive() {
-      // 使用 GSAP 实现弹性变形到活动状态
-      this.menuState = "breadCrumb"
-      gsap.to(".menu", { duration: 0.5, height: "60px", ease: "elastic.out(1, 0.5)" });
-    },
-    animateToDefault() {
+
       // 使用 GSAP 实现弹性变形回到默认状态
-      gsap.to(".menu", { duration: 0.5, height: "80px", ease: "elastic.out(1, 0.5)" });
+      gsap.to(".menu", {
+        duration: 0.5,
+        width: "calc-size(auto)",
+        ease: "elastic.out(1, 0.5)"
+      });
     },
   },
 };
@@ -59,6 +63,8 @@ export default {
 
 <style>
 .menu {
+  /* interpolate-size: allow-keywords; */
+  /* transition: 0.5s; */
   position: fixed;
   bottom: 20px;
   left: 50%;
